9da964d2176cdf104a16d329a3c39084c7e89c82
Guido Draheim <guidod@gmx.de>
2026-02-21 19:57:33 +0100

    style

--- git_fast_import_merge.tests.py
+++ git_fast_import_merge.tests.py
@@ -2349,7 +2349,7 @@ class ImportMergeTest(TestCase):
         std = sh(F"{cover} {merge} A.fi B2.fi -o N2.fi --merge --into=N", cwd=tmp)
         self.assertTrue(greplines(std.out, ""))
         catN2 = sh_cat(F"N2.fi", cwd=tmp)
-        logg.log("catN2\n%s", catN2.out)
+        logg.warning("catN2\n%s", catN2.out)
         self.assertFalse(greplines(catN2.out, "hello-A", "hello-B"))  # skipped old patches
         self.assertFalse(greplines(catN2.out, "merge :"))  # and 'updated' has no merge
         #
@@ -2678,7 +2678,7 @@ class ImportMergeTest(TestCase):
         self.assertTrue(greplines(std.out, COMMITHASH))
         head = std.out.strip()
         std = sh(F"{git} show -s --format=%cI HEAD", cwd=N)
-        logg.log("show -s\n%s", std.out)
+        logg.warning("show -s\n%s", std.out)
         self.assertTrue(greplines(std.out, TIMEFORMAT))
         date = std.out.strip()
         std = sh(F"{cover} {merge} A.fi B2.fi -o N2.fi --merge --head={head} --date={date}", cwd=tmp)
